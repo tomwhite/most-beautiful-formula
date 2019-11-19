7a150c7ba8dfef8cb01d8938cec08bf9a418d5f4
Tom White <tom.e.white@gmail.com>
2019-11-19 10:15:46 +0000

    Experiment with labels

--- eipi.js
+++ eipi.js
@@ -30,3 +30,39 @@ function getPointsAsPath(points, scale) {
   });
   return d;
 }
+
+function getPointsAsLabels(points, scale) {
+  var labels = [];
+  points.forEach(function(p, index, array) {
+    if (index > 0) {
+      var prevP = points[index - 1];
+      var midx = scale * (p.x + prevP.x) / 2;
+      var midy = scale * (p.y + prevP.y) / 2;
+      var len = scale * Math.abs((index % 2 == 0) ? p.y - prevP.y : p.x - prevP.x);
+      var nudgex = 0;
+      var nudgey = 0;
+      switch ((index - 1) % 4) {
+        case 0:
+          nudgey = len / 10;
+          break;
+        case 1:
+          nudgex = len / 10;
+          break;
+        case 2:
+          nudgey = -len / 10;
+          break;
+        case 3:
+          nudgex = -len / 10;
+          break;
+        default:
+      }
+      if (index == 1) {
+        nudgey = len / 5; // special case
+      }
+      midx += nudgex;
+      midy += nudgey;
+      labels.push({i: index - 1, midx: midx, midy: midy, len: len});
+    }
+  });
+  return labels;
+}
